861dd765f9c15289bade37b58f6e90a5ead0e90a
coalong <minay0904@gmail.com>
2022-10-26 21:01:31 +0900

    기능 1. 구현 commit

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,21 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        for (int i = 1; i <= number; i++) {
+            answer += count369(i);
+        }
         return answer;
+    }
+
+    static int count369(int num) {
+        int cnt = 0;
+        while (num != 0) {
+            if (num % 10 == 3 || num % 10 == 6 || num % 10 == 9) {
+                cnt++;
+            }
+            num /= 10;
+        }
+        return cnt;
     }
 }
