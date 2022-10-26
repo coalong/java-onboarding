27aa955e57ab412c78ec08a0e7fd8ca1bcf18f32
coalong <minay0904@gmail.com>
2022-10-26 17:28:07 +0900

    기능 1. 구현 commit

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,16 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        if (checkPageValid(pobi) || checkPageValid(crong)) {
+            return -1;
+        }
+
+
+        return 0;
     }
+
+    static boolean checkPageValid(List<Integer> pageList) {
+        return (pageList.get(1) - pageList.get(0) != 1) || pageList.get(0) % 2 != 1 || pageList.get(1) % 2 != 0;
+    }
+
 }
