0cb4e9e5f28d08b3339a0f41b01f9c1d467c582a
coalong <minay0904@gmail.com>
2022-10-26 18:05:43 +0900

    기능 2. 구현 commit

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,13 +7,27 @@ class Problem1 {
         if (checkPageValid(pobi) || checkPageValid(crong)) {
             return -1;
         }
-
-
         return 0;
     }
 
-    static boolean checkPageValid(List<Integer> pageList) {
+    private static boolean checkPageValid(List<Integer> pageList) {
         return (pageList.get(1) - pageList.get(0) != 1) || pageList.get(0) % 2 != 1 || pageList.get(1) % 2 != 0;
+    }
+
+    private static int calHighPage(int oddNum, int evenNum) {
+        return Math.max(calHighScore(oddNum), calHighScore(evenNum));
+    }
+
+    private static int calHighScore(int pageNum) {
+        int sum = 0;
+        int mul = 1;
+
+        while (pageNum != 0) {
+            sum += pageNum % 10;
+            mul *= pageNum % 10;
+            pageNum /= 10;
+        }
+        return Math.max(sum, mul);
     }
 
 }
