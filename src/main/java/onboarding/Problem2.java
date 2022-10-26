ab48b7181b5008d89d371a72c2fb8efb12c292e8
coalong <minay0904@gmail.com>
2022-10-26 20:28:46 +0900

    기능 1. 구현 commit

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,28 @@
 package onboarding;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        StringBuilder answer = new StringBuilder();
+        LinkedList<Character> cryptogramChar = new LinkedList<>(List.of('A'));
+        int index = 0;
+
+        for (int i = 0; i < cryptogram.length(); i++) {
+            if (cryptogramChar.get(index).equals(cryptogram.charAt(i))) {
+                cryptogramChar.remove(index);
+                index--;
+            } else {
+                cryptogramChar.add(cryptogram.charAt(i));
+                index++;
+            }
+        }
+
+        cryptogramChar.remove(0);
+        for (Character character : cryptogramChar) {
+            answer.append(character);
+        }
+        return answer.toString();
     }
 }
