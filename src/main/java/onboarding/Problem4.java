840ab38f60839628ac3e34464399fe9a11542ec2
coalong <minay0904@gmail.com>
2022-10-27 12:39:00 +0900

    기능 구현 commit

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,23 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        StringBuilder answer = new StringBuilder();
+        char[] wordSplit = word.toCharArray();
+        for (char token : wordSplit) {
+            if (Character.isUpperCase(token)) {
+                answer.append(convertUpperAlphabet(token));
+            } else if (Character.isLowerCase(token)) {
+                answer.append(convertLowerAlphabet(token));
+            } else answer.append(token);
+        }
+        return answer.toString();
+    }
+
+    static char convertUpperAlphabet(char token) {
+        return (char)(25 - (token - 65) + 65);
+    }
+
+    static char convertLowerAlphabet(char token) {
+        return (char)(25 - (token - 97) + 97);
     }
 }
