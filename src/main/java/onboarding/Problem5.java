78ff90767bf9900b812b97df62717e943fbf4d43
coalong <minay0904@gmail.com>
2022-10-27 12:44:55 +0900

    기능 2. 구현 commit

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -7,7 +7,12 @@ public class Problem5 {
         List<Integer> moneyUnit = List.of(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
         List<Integer> answer = new java.util.ArrayList<>(List.of(0, 0, 0, 0, 0, 0, 0, 0, 0));
 
-
+        for (int i = 0; i < answer.size(); i++) {
+            if (money / moneyUnit.get(i) >= 1) {
+                answer.set(i, money / moneyUnit.get(i));
+                money %= moneyUnit.get(i);
+            }
+        }
         return answer;
     }
 
